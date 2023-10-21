404c8fc6e925051c641f3a3d38ea580967ec3be7
xeedis <k.tarnowski99@gmail.com>
2023-10-21 21:28:39 +0200

    First commit

---
+++ Converter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Bin2Dec
+{
+    public class Converter
+    {
+        public static string ToDecimal(string binary)
+        {
+            if (!IsBinaryString(binary)) return "Wrong input given!";
+            var temp = binary.ToArray();
+            Array.Reverse(temp);
+            var reversed = new string(temp);
+            int result = 0;
+            int actualNumber = 0;
+            for(int i = 0; i < reversed.Length; ++i)
+            {
+                actualNumber = reversed[i] - '0';
+                result += actualNumber * (int)Math.Pow(2, i);
+            }
+
+            return result.ToString();
+        }
+
+        private static bool IsBinaryString(string binary)
+        {
+            return Regex.IsMatch(binary, "^[01]+$");
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Bin2Dec;
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+        //textInput.TextChanged += TextInput_TextChanged;
+    }
+
+    private void TextInput_TextChanged(object sender, TextChangedEventArgs e)
+    {
+        liveText.Text = Converter.ToDecimal(textInput.Text);
+    }
+}
